90416af7fb6adb230d5796012b2f47550b3cee10
Pale <pporob00@fesb.hr>
2017-01-22 11:04:04 +0100

    prvi commit

--- app/src/main/java/com/example/korisnik/nfcreader/MainActivity.java
+++ app/src/main/java/com/example/korisnik/nfcreader/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.korisnik.nfcreader;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import be.appfoundry.nfclibrary.activities.NfcActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends NfcActivity {
+
+    private PendingIntent pendingIntent;
+    private IntentFilter[] mIntentFilters;
+    private String[][] mTechLists;
+    private NfcAdapter mNfcAdapter;
+
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // enableBeam();
     }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        for (String message : getNfcMessages()){
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+        }
+        Log.d("Intent", "intent started...");
+    }
+
+
+
 }
